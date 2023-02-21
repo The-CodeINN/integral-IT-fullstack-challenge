05ddcebabbd69058f4162426466627c6cfa90e31
Richard Emijere <emijere.richard@gmail.com>
2023-02-21 00:27:17 +0000

    Done with task_1

---
+++ task_1/application-form/README.md
@@ -0,0 +1,28 @@
+# Software Engineering Role Application Form
+
+This is a simple web form built with HTML, CSS, and JavaScript that collects information from candidates applying for a software engineering role.
+
+## Features
+
+The form collects the following information:
+
+- First name
+- Last name
+- Email address
+- Phone number
+- Portfolio link
+- Why the candidate should be hired
+
+When a user submits the form, their information is stored in a Google Sheet. The URL of the sheet is printed on the page so that the user can see the data that they have submitted.
+
+## Getting Started
+
+To use the form, simply open the `index.html` file in a web browser. Fill out the form with your information and click the "Submit" button. You should see a success message if the form was submitted successfully.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- [SheetDB](https://sheetdb.io/) - API for storing form responses in a Google Sheet
+- [SweetAlert](https://sweetalert.js.org/) - JavaScript library for displaying alert messages
--- task_1/application-form/main.js
+++ task_1/application-form/main.js
@@ -1,7 +1,64 @@
-import './style.css'
+import "./style.css";
+import swal from "sweetalert";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Application for Software Engineering Role</h1>
+document.querySelector("#app").innerHTML = `
+<div>
+<h1 class="title">Application for Software Engineering Role</h1>
+<form
+  action="https://sheetdb.io/api/v1/xuwu66iw1sbk8"
+  method="post"
+  id="sheetdb-form"
+  class="form"
+>
+  <div class="form-group">
+    <label for="firstName" class="label">First Name:</label>
+    <input type="text" id="firstName" name="data[firstName]" class="input" />
   </div>
-`
+
+  <div class="form-group">
+    <label for="lastName" class="label">Last Name:</label>
+    <input type="text" id="lastName" name="data[lastName]" class="input" />
+  </div>
+
+  <div class="form-group">
+    <label for="email" class="label">Email:</label>
+    <input type="email" id="email" name="data[email]" class="input" />
+  </div>
+
+  <div class="form-group">
+    <label for="phone" class="label">Phone Number:</label>
+    <input type="tel" id="phone" name="data[phone]" class="input" />
+  </div>
+
+  <div class="form-group">
+    <label for="portfolio" class="label">Portfolio Link:</label>
+    <input type="url" id="portfolio" name="data[portfolio]" class="input" />
+  </div>
+
+  <div class="form-group">
+    <label for="whyHire" class="label">Why should we hire you?:</label>
+    <textarea id="whyHire" name="data[whyHire]" class="input"></textarea>
+  </div>
+
+  <button type="submit" class="button">Submit</button>
+</form>
+</div>
+`;
+
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  })
+    .then((response) => response.json())
+    .then((html) => {
+      swal(
+        "Great!",
+        "Form submitted successfully!",
+        "success"
+      );
+    });
+});
