6a40ae9b8aee470337408e19876c1032e0db802c
Richard Emijere <emijere.richard@gmail.com>
2023-02-22 10:30:17 +0000

    Tailwind setup with React

--- task_5/events/src/components/Navbar..jsx
+++ task_5/events/src/components/Navbar..jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return(
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      Navbar
+    </div>
+  )
 };
 
 export default Navbar;
